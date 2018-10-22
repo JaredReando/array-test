2b4b01c6063e4f2e67a3a3c0f379a72bfec60052
Jared Reando and kohichi horii <jared@>
2018-10-22 13:04:53 -0700

    created arraytest, add .push with arrays showing in the html

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+var newArray = [];
+
+
+
+$(document).ready(function() {
+  $("#favorites").submit(function(event) {
+    event.preventDefault();
+
+    var color = $("#color").val();
+    var animal = $("#animal").val();
+    var snack = $("#snack").val();
+
+    newArray.push(color, animal, snack);
+    newArray = [color, animal, snack];
+    console.log(newArray);
+
+    newArray2 = [newArray[1].slice(), newArray[0].slice(), newArray[2].slice()];
+
+    $("#arrayInsert1").text(newArray2[0]);
+    $("#arrayInsert2").text(newArray2[2]);
+    console.log(newArray2);
+    // newArray.push(animal);
+    // newArray.push(snack);
+
+
+
+    // newArray = newArray.push[color,animal,snack];
+    // console.log(newArray);
+  });
+});
